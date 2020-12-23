f757daae799da94979a6920b10626e65c29d2b34
Tony <kakaopage@kakaopageui-MacBookPro.local>
2020-12-23 17:37:48 +0900

    change commit

---
+++ todo-list/src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-footer
+        v-if="todos.length > 0"
+        color="success lighten-1"
+        rounded>
+        <v-row
+            justify="center"
+            no-gutters>
+            <div class="my-3" style="color: white; font-size: 0.9rem; text-align: center">{{todos.length - completedTodos}} items left</div>
+
+            <v-spacer></v-spacer>
+
+            <v-btn href="#/all" @click="$emit('changeVisibility', 'all')" color="white" rounded text class="my-1" style="font-size: 0.75rem">
+                All
+            </v-btn>
+            <v-btn href="#/active" @click="$emit('changeVisibility', 'active')" color="white" rounded text class="my-1" style="font-size: 0.75rem">
+                Active
+            </v-btn>
+            <v-btn href="#/completed" @click="$emit('changeVisibility', 'completed')" color="white" rounded text class="my-1" style="font-size: 0.75rem">
+                Completed
+            </v-btn>
+
+            <v-spacer></v-spacer>
+            <v-btn v-show="clearCompletedShow(todos)" @click="$emit('clearCompletedTodos')" color="white" rounded text class="my-1" style="font-size: 0.75rem">
+                Clear completed
+            </v-btn>
+        </v-row>
+    </v-footer>
+</template>
+
+<script>
+
+var filters = {
+    all: function (todos) {
+        return todos;
+    },
+    active: function (todos) {
+        return todos.filter(function (todo) {
+            return !todo.checked;
+        });
+    },
+    completed: function (todos) {
+        return todos.filter(function (todo) {
+            return todo.checked;
+        });
+    }
+}
+
+export default {
+    name: "Footer",
+    props: ['todos', 'completedTodos'],
+    methods: {
+        clearCompletedShow: function (todos) {
+            return todos.length > filters.active(todos).length;
+        }
+    }
+}
+</script>
